f2277cd86571476e2490942c283429e8c682a4e2
Arseniy <73026795+arseniy89@users.noreply.github.com>
2022-02-19 22:55:45 +0300

    Update connect_four.py

--- connect_four.py
+++ connect_four.py
@@ -1,64 +1,58 @@
-def possibility():
-  if (pos_i == 5):
-    return True
-  if (board[pos_i+1][pos_j] != "  "):
-    return True
-  return False 
+from itertools import product
+
+
+def cout():
+    [print(f'    {i+1}', end='') for i in range(6)]
+    print(s:=(f"\n  {'●━━━━'*6}●"))
+    [print(f'{i+1} ┃ {b[i][0]} ┃ {b[i][1]} ┃ {b[i][2]} ┃ {b[i][3]} ┃ {b[i][4]} ┃ {b[i][5]} ┃ {i+1}{s}') 
+    for i in range(6)]
+    [print(f'    {i+1}', end='') for i in range(6)]
+    print()
+
+
+def possibility(i, j):
+    return (i == 5 or b[i+1][j] != '  ') and b[i][j] == '  '
+
+
+def reading(step):
+    while 1:
+        try:
+            i = int(input('2nd: ' if (step%2) else '1st: ')) - 1
+            j = int(input('2nd: ' if (step%2) else '1st: ')) - 1
+            if possibility(i, j): return [i, j]
+        except: continue
+
 
 def check():
-  for i in range(6):
-    for j in range(3):
-      if (board[i][j] == board[i][j+1] ==
-      board[i][j+2] == board[i][j+3] != "  "):
-        return True
-  for j in range(6):
-    for i in range(3):
-      if (board[i][j] == board[i+1][j] ==
-      board[i+2][j] == board[i+3][j] != "  "):
-        return True   
-  for i in range(0, 3):
-    for j in range(0, 3):
-      if (board[i][j] == board[i+1][j+1] ==
-      board[i+2][j+2] == board[i+3][j+3] != "  "):
-        return True
-  for i in range(0, 3):
-    for j in range(3, 6):
-      if (board[i][j] == board[i+1][j-1] ==
-      board[i+2][j-2] == board[i+3][j-3] != "  "):
-        return True
-  return False
-   
-def cout():
-  for i in range(6):
-    print(" "*4 + str(i+1), end="")
-  print(), print(" "*2 + "+----"*6 + "+")
-  for i in range(6):
-    print(i+1, end= " ")
-    for j in range(6):
-      print("| " + board[i][j], end=" ")
-    print("| " + str(i+1))
-    print(" "*2 + "+----"*6 + "+")
-  for i in range(6):
-    print(" "*4 + str(i+1), end="")
-  print()
+    for i,j in product(range(6), range(3)):
+            if (b[i][j] == b[i][j+1] == b[i][j+2] == b[i][j+3] != '  '):
+                return True
+    for j,i in product(range(6), range(3)):
+            if (b[i][j] == b[i+1][j] == b[i+2][j] == b[i+3][j] != '  '):
+                return True   
+    for i,j in product(range(3), range(3)):
+            if (b[i][j] == b[i+1][j+1] == b[i+2][j+2] == b[i+3][j+3] != '  '):
+                return True
+    for i,j in product(range(3), range(3, 6)):
+            if (b[i][j] == b[i+1][j-1] == b[i+2][j-2] == b[i+3][j-3] != '  '):
+                return True
+    return False
 
-print("This is the game connect four:")
-count, board = 36, [["  "]*6 for i in range(6)]
-cout()
 
-while(count):
-  pos_i = int(input(("First" if (count % 2 == 0) else "Second") + 
-  " player, enter the line number:")) - 1
-  pos_j = int(input(("First" if (count % 2 == 0) else "Second") + 
-  " player, enter the column number:")) - 1
-  if (possibility()):
-    board[pos_i][pos_j] = "x " if (count % 2 == 0) else "o "
-    cout()
-    if (check()): break
-    count -= 1
+def main():
+    print('Connect Four game:')
+    while input('Play? (y) ') == 'y':
+        global b; b = [['  ']*6 for _ in range(6)]
+        cout()
+        for step in range(36):
+            [i, j] = reading(step)
+            b[i][j] = 'o ' if (step%2) else 'x '
+            cout()
+            if check(): break
+        if check():
+            print(('2nd' if (step%2) else '1st') + ' won')
+        else:
+            print('Friendship won')
 
-if (count > 0):
-  print(("First" if (count % 2 == 0) else "Second") + 
-  " player won")
-else:
-  print("Friendship won")
+
+main()
